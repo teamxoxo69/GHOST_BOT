b410666c4188e94aa50d6c3b5aa7cde20255b86a
teamxoxo69 <jara56907@gmail.com>
2024-07-06 10:09:14 +0600

    Update reaply.js

--- scripts/commands/reaply.js
+++ scripts/commands/reaply.js
@@ -1,36 +1,33 @@
 const fs = require("fs");
+
 module.exports.config = {
-	name: "hasi2",
+    name: "hasi2",
     version: "1.1.1",
-	hasPermssion: 0,
-	credits: "Pixxi Foysal", 
-	description: "Just Respond",
-	commandCategory: "no prefix",
-    cooldowns: 5, 
+    hasPermssion: 0,
+    credits: "Pixxi Foysal",
+    description: "Just Respond",
+    commandCategory: "no prefix",
+    cooldowns: 5,
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("😘") ||
-     react.includes("😽") || 
-react.includes("😘") || 
-react.includes("💋") ||
-react.includes("👅") ||
-react.includes("👄") ||
-react.includes("😙") ||
-react.includes("😚") ||
-react.includes("😙") ||
-react.includes("💋") ||
-react.includes("👩‍❤️‍💋‍👨") ||
-react.includes("🫦")) {
-		var msg = {
-				body: "--এ্ই্ঁ বে্ঁডা্ঁ এ্ঁই্ঁস্ঁব্ঁ চু্ঁমা্ঁ চা্ঁপ্ঁটি্ঁ এ্ঁক্ঁদো্ঁম্ঁ ভা্ঁলা্ঁ লা্ঁগে্ঁ না্ঁহ্ঁ_!!🙈"
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+    var { threadID, messageID } = event;
+    let react = event.body.toLowerCase();
+    if (react.includes("😘") ||
+        react.includes("😽") ||
+        react.includes("💋") ||
+        react.includes("👅") ||
+        react.includes("👄") ||
+        react.includes("😙") ||
+        react.includes("😚") ||
+        react.includes("👩‍❤️‍💋‍👨") ||
+        react.includes("🫦")) {
+        var msg = {
+            body: "--এ্ই্ঁ বে্ঁডা্ঁ এ্ঁই্ঁস্ঁব্ঁ চু্ঁমা্ঁ চা্ঁপ্ঁটি্ঁ এ্ঁক্ঁদো্ঁম্ঁ ভা্ঁলা্ঁ লা্ঁগে্ঁ না্ঁহ্ঁ_!!🙈"
+        };
+        api.sendMessage(msg, threadID, messageID);
+        api.setMessageReaction("🙈", messageID, (err) => {}, true);
+    }
+};
 
-	}
+module.exports.run = function({ api, event, client, __GLOBAL }) {};
